71ddb81e461e077d6b74b5dac622acea9187ede3
Cristian Rodriguez <ilovehouki@gmail.com>
2023-09-02 10:02:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,38 @@
-import { Client } from 'osu-web.js';
+const axios = require('axios'); // Import axios or your preferred HTTP library
 
-// Initialize the client with your Osu! API key
-const client = new Client('4de9a885c54d8daac6b9c7245f453ad317f644dc'); // Replace with your API key
+// Your GitHub OAuth application credentials
+const clientID = '24416';
+const clientSecret = 'Tl76xxdI7osW6wSpQcTYZ9p1qILcZiFUCXVeckMo';
 
-// Function to fetch and display user data
-async function getUserData(username) {
-  try {
-    // Fetch user data using the 'getUser' method
-    const userData = await client.users.getUser(username, {
-      urlParams: {
-        mode: 'osu', // Specify the game mode (e.g., 'osu', 'taiko', 'mania', 'fruits')
-      },
-    });
+// The URL to GitHub's OAuth token endpoint
+const tokenURL = 'https://github.com/login/oauth/access_token';
 
-    // Display the user data in the HTML
-    const userDataDiv = document.getElementById('user-data');
-    userDataDiv.innerHTML = `
-      <h2>User Data for ${username}</h2>
-      <p>Osu! ID: ${userData.id}</p>
-      <p>Username: ${userData.username}</p>
-      <p>PP: ${userData.statistics.pp}</p>
-      <!-- Add more user data fields as needed -->
-    `;
+// Redirect URI - should match the one you configured on GitHub
+const redirectURI = 'https://github.com/ayxse/osu-program';
 
-    console.log('User Data:', userData); // Log the user data for debugging
+// The code received after successful GitHub OAuth authentication
+const code = 'code_received_from_oauth_callback';
 
-  } catch (error) {
-    // Handle any errors that occur during the API request
-    console.error('Error fetching user data:', error);
+// Exchange the code for an access token
+axios
+  .post(tokenURL, null, {
+    params: {
+      client_id: clientID,
+      client_secret: clientSecret,
+      code: code,
+      redirect_uri: redirectURI,
+    },
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+  .then((response) => {
+    const accessToken = response.data.access_token;
+    console.log('Access Token:', accessToken);
 
-    // Display an error message in the HTML
-    const userDataDiv = document.getElementById('user-data');
-    userDataDiv.innerHTML = '<p>Error fetching user data. Please try again.</p>';
-  }
-}
-
-// Event listener for the form submission
-const osuForm = document.getElementById('osu-form');
-
-osuForm.addEventListener('submit', function (e) {
-  e.preventDefault(); // Prevent the default form submission behavior
-
-  // Get the input value (Osu! username)
-  const username = document.getElementById('osu-username').value;
-
-  // Call the function to fetch user data using the entered username
-  getUserData(username);
-});
+    // Now that you have the access token, you can make authenticated requests to the GitHub API.
+    // You can use this token to fetch user data or perform other actions on behalf of the user.
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
